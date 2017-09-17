192fb1f44eabda55973ca166f512bbac48be48a0
unknown <miccel88@gmail.com>
2017-09-18 00:07:19 +0200

    Inizio progetto Crash Bash

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+	public float speed = 3f;
+	private Transform _transform;
+	private Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+		_transform = transform;
+		position = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		_transform.Translate(Input.GetAxisRaw("Horizontal")*speed*Time.deltaTime,0,0);
+	}
+}
